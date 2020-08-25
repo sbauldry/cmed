f61aead0a6da34acf0c89f8304e45456d3edfc10
Shawn Bauldry <sgbauldry@gmail.com>
2020-08-25 13:34:55 -0400

    add past mental health

--- cmed-data.R
+++ cmed-data.R
@@ -17,11 +17,14 @@ pew1 <- read_stata("PEWCOVID.dta")
 ### analysis sample: age 65+, white/black/latinx
 vars <- c("f_agecat", "f_sex", "f_racethn", "f_marital", "covidfol_w64", "mh_track_a_w64", 
           "mh_track_b_w64", "mh_track_c_w64", "mh_track_d_w64", "mh_track_e_w64", "weight_w64",
-          "f_metro", "f_cregion", "f_educcat")
+          "f_metro", "f_cregion", "f_educcat", "covid_mental_w64")
 pew2 <- pew1 %>%
   select(all_of(vars)) %>%  
   filter(f_agecat == 4) %>% 
   filter(f_racethn < 4) 
+
+### initial sample size
+length(pew2$f_agecat)
 
 ### preparing variables for analysis
 ### Marital status: "married" and "living with partner" (MP)
@@ -36,20 +39,24 @@ pew3 <- pew2 %>%
          met = recode(as_factor(f_metro), "Metropolitan" = "M", "Non-metropolitan" = "nM"),
          reg = recode(as_factor(f_cregion), "Northeast" = "N", "Midwest" = "M", "South" = "S", "West" = "W"),
          edu = recode(as_factor(f_educcat), "College graduate+" = "BA+", "Some College" = "SC", "H.S. graduate or less" = "HS-"),
+         psy = recode(as_factor(covid_mental_w64), "Yes" = "Y", "No" = "N"),
          mar = ifelse(f_marital < 3, 1, 0),
          mar = recode(as_factor(mar), "1" = "MP", "0" = "nMP"),
          nws_n = ifelse(covidfol_w64 == 1, 1, 0),
          nws = recode(as_factor(nws_n), "1" = "VC", "0" = "nVC"),
          dis = mh_track_a_w64 + mh_track_b_w64 + mh_track_c_w64 + (5 - mh_track_d_w64) + mh_track_e_w64,
          wgt = weight_w64) %>%
-  select(c(sex, rac, met, reg, edu, mar, nws_n, nws, dis, wgt))
+  select(c(sex, rac, met, reg, edu, mar, psy, nws_n, nws, dis, wgt))
 summary(pew3)
 
 ### check missing data
-### listwise delete due to minimal missing data (< 6 for covariates and 37 for psychological distress)
+### listwise delete due to minimal missing data (< 15 for covariates and 37 for psychological distress)
 summary(pew3)
 pew4 <- pew3 %>%
   drop_na()
+
+### analysis sample size
+length(pew4$sex)
 
 
 ### save data for analysis
