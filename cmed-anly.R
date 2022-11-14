eac4daf7dc41a4325ae00cf0b445d4e96086bb57
sgbauldry@gmail.com <sgbauldry@gmail.com>
2022-11-14 16:55:08 -0500

    R&R update 1

--- cmed-anly.R
+++ cmed-anly.R
@@ -1,6 +1,6 @@
 ### Purpose: Prepare analysis of covid media consumption and distress
 ### Author:  S Bauldry
-### Date:    August 15, 2022
+### Date:    September 5, 2022
 
 setwd("~/desktop")
 library(tidyverse)
@@ -11,9 +11,9 @@ library(weights)
 
 
 ### load prepared data for analysis
-pew_old <- read_csv("cmed-old-data.csv", 
-                    col_types = list(nws = "f", rce = "f", fem = "f", met = "f",
-                                     reg = "f", mar = "f", edu = "f", mhc = "f")) %>%
+pew <- read_csv("cmed-old-data.csv", 
+                col_types = list(nws = "f", rce = "f", fem = "f", met = "f",
+                                 reg = "f", mar = "f", edu = "f", mhc = "f")) %>%
   mutate(nws = fct_relevel(nws, "0", "1"),
          rce = fct_relevel(rce, "1", "2", "3"),
          fem = fct_relevel(fem, "0", "1"),
@@ -25,22 +25,52 @@ pew_old <- read_csv("cmed-old-data.csv",
 
 
 ### descriptive statistics (Table 1)
+pew_vc <- pew %>%
+  filter(nws == "1")
+
+pew_nvc <- pew %>%
+  filter(nws == "0")
+
+# overall sample size and distribution of media exposure
+length(pew$dis)
+table(pew$nws)
+wpct(pew$nws, weight = pew$rwt)
+
+# psychological distress
+mn_dis <- wtd.mean(pew$dis, weights = pew$rwt)
+sd_dis <- sqrt( wtd.var(pew$dis, weights = pew$rwt) )
+
+mn_dis_vc <- wtd.mean(pew_vc$dis, weights = pew_vc$rwt)
+sd_dis_vc <- sqrt( wtd.var(pew_vc$dis, weights = pew_vc$rwt) )
+
+mn_dis_nvc <- wtd.mean(pew_nvc$dis, weights = pew_nvc$rwt)
+sd_dis_nvc <- sqrt( wtd.var(pew_nvc$dis, weights = pew_nvc$rwt) )
+
+c(mn_dis, sd_dis, mn_dis_vc, sd_dis_vc, mn_dis_nvc, sd_dis_nvc)
+
+# other covariates
+pr_var <- function(x, y, z) {
+  a <- wpct(x, weight = pew$rwt)
+  b <- wpct(y, weight = pew_vc$rwt)
+  c <- wpct(z, weight = pew_nvc$rwt)
+  c(a, b, c)
+}
+
+pr_var(pew$fem, pew_vc$fem, pew_nvc$fem)
+pr_var(pew$rce, pew_vc$rce, pew_nvc$rce)
+pr_var(pew$edu, pew_vc$edu, pew_nvc$edu)
+pr_var(pew$mar, pew_vc$mar, pew_nvc$mar)
+pr_var(pew$met, pew_vc$met, pew_nvc$met)
+pr_var(pew$reg, pew_vc$reg, pew_nvc$reg)
+pr_var(pew$mhc, pew_vc$mhc, pew_nvc$mhc)
 
 
 
-### predictors of very close media consumption (Table 2)
-nws_m1 <- glm(nws ~ rce + fem + met + reg + mar + edu + mhc, data = pew_old, 
-               family = quasibinomial, weights = rwt)
-summary(nws_m1)
-summary( margins(nws_m1) )
-
-
-### average net effect of very close media consumption (Table 3)
-dis_m1 <- lm(dis ~ nws + rce + fem + edu + mar + met + reg + mhc, data = pew_old, 
+### average net effect of very close media consumption (Figure 1)
+dis_m1 <- lm(dis ~ nws + rce + fem + edu + mar + met + reg + mhc, data = pew, 
              weights = rwt) 
 summary(dis_m1)
 
-# graph estimates for presentations
 dis_m1_est <- tidy(dis_m1) %>%
   filter(term != "(Intercept)") %>%
   mutate(lb = estimate - 1.96*std.error,
@@ -59,29 +89,30 @@ fig1 <- ggplot(dis_m1_est, aes(x = factor(id), y = estimate, ymin = lb, ymax = u
   theme_light() +
   theme(text = element_text(size = 15))
 fig1
-ggsave("cmed-fig1.png", plot = fig1)
+ggsave("cmed-fig1.pdf", plot = fig1)
 
 
-### subgroup estimates for very close media consumption
+### subgroup estimates for very close media consumption (Figure 2)
 str_mod <- function(v, i) {
-  d <- pew_old %>% filter( v == i )
-  m <- lm(dis ~ nws + met + reg + mhc + fem1 + rce2 + rce3 + edu2 + edu3 + mar1, data = d, weights = rwt)
+  d <- pew %>% filter( v == i )
+  m <- lm(dis ~ nws + met + reg + mhc + fem1 + rce2 + rce3 + edu2 + edu3 + 
+                mar1, data = d, weights = rwt)
   return(m)
 }
 
-m_mal <- str_mod(pew_old$fem, 0)
-m_fem <- str_mod(pew_old$fem, 1)
+m_mal <- str_mod(pew$fem, 0)
+m_fem <- str_mod(pew$fem, 1)
 
-m_hsp <- str_mod(pew_old$rce, 1)
-m_blk <- str_mod(pew_old$rce, 2)
-m_wht <- str_mod(pew_old$rce, 3)
+m_hsp <- str_mod(pew$rce, 1)
+m_blk <- str_mod(pew$rce, 2)
+m_wht <- str_mod(pew$rce, 3)
 
-m_hs  <- str_mod(pew_old$edu, 1)
-m_sc  <- str_mod(pew_old$edu, 2)
-m_ba  <- str_mod(pew_old$edu, 3)
+m_hs  <- str_mod(pew$edu, 1)
+m_sc  <- str_mod(pew$edu, 2)
+m_ba  <- str_mod(pew$edu, 3)
 
-m_nmc <- str_mod(pew_old$mar, 0)
-m_mc  <- str_mod(pew_old$mar, 1)
+m_nmc <- str_mod(pew$mar, 0)
+m_mc  <- str_mod(pew$mar, 1)
 
 # gathering estimates of very close media consumption
 ge <- function(mod, i) {
@@ -92,9 +123,9 @@ ge <- function(mod, i) {
   c <- cbind(id = i, est = e[[2]], se = s[[2]], lb = l, ub = u)
   return(c)
 }
-est <- data.frame( rbind( ge(m_fem, 10), ge(m_mal, 9), ge(m_hsp, 8), ge(m_blk, 7), 
-                          ge(m_wht, 6), ge(m_ba, 5), ge(m_sc, 4), ge(m_hs, 3),
-                          ge(m_mc, 2), ge(m_nmc, 1) ) )
+est <- data.frame(rbind( ge(m_fem, 10), ge(m_mal, 9), ge(m_hsp, 8), 
+                         ge(m_blk, 7), ge(m_wht, 6), ge(m_ba, 5), 
+                         ge(m_sc, 4), ge(m_hs, 3), ge(m_mc, 2), ge(m_nmc, 1) ))
 
 # Clogg test for pairwise contrasts
 dt <- function(i, j) {
@@ -120,7 +151,7 @@ dt(9, 10) # mar - not mar
 prd_dis <- function(v, i) {
   
   # select subsample
-  d <- pew_old %>% filter( v == i )
+  d <- pew %>% filter( v == i )
   
   # create vector of weighted means
   wmv <- c(weighted.mean(d$mar1, d$rwt), weighted.mean(d$met1, d$rwt),
@@ -147,24 +178,24 @@ prd_dis <- function(v, i) {
   return(prp)
 }
 
-pr_fem <- prd_dis(pew_old$fem, 1)
-pr_mal <- prd_dis(pew_old$fem, 0)
-pr_hsp <- prd_dis(pew_old$rce, 1)
-pr_blk <- prd_dis(pew_old$rce, 2)
-pr_wht <- prd_dis(pew_old$rce, 3)
-pr_hs  <- prd_dis(pew_old$edu, 1)
-pr_sc  <- prd_dis(pew_old$edu, 2)
-pr_ba  <- prd_dis(pew_old$edu, 3)
-pr_nmc <- prd_dis(pew_old$mar, 0)
-pr_mc  <- prd_dis(pew_old$mar, 1)
+pr_fem <- prd_dis(pew$fem, 1)
+pr_mal <- prd_dis(pew$fem, 0)
+pr_hsp <- prd_dis(pew$rce, 1)
+pr_blk <- prd_dis(pew$rce, 2)
+pr_wht <- prd_dis(pew$rce, 3)
+pr_hs  <- prd_dis(pew$edu, 1)
+pr_sc  <- prd_dis(pew$edu, 2)
+pr_ba  <- prd_dis(pew$edu, 3)
+pr_nmc <- prd_dis(pew$mar, 0)
+pr_mc  <- prd_dis(pew$mar, 1)
 
 # gathering estimates of predicted psychological distress
 id <- 1:29
-prdis <- data.frame( cbind( id, rbind(pr_nmc, c(0, 0, 0), pr_mc, c(0, 0, 0), 
-                                      pr_hs, c(0, 0, 0), pr_sc, c(0, 0, 0), 
-                                      pr_ba, c(0, 0, 0), pr_wht, c(0, 0, 0), 
-                                      pr_blk, c(0, 0, 0), pr_hsp, c(0, 0, 0), 
-                                      pr_mal, c(0, 0, 0), pr_fem) ) )
+prdis <- data.frame(cbind( id, rbind(pr_nmc, c(0, 0, 0), pr_mc, c(0, 0, 0), 
+                                     pr_hs, c(0, 0, 0), pr_sc, c(0, 0, 0), 
+                                     pr_ba, c(0, 0, 0), pr_wht, c(0, 0, 0), 
+                                     pr_blk, c(0, 0, 0), pr_hsp, c(0, 0, 0), 
+                                     pr_mal, c(0, 0, 0), pr_fem) ))
 colnames(prdis) <- c("id", "nws", "est", "se")
 
 # graph estimates and predicted values
@@ -172,14 +203,15 @@ fig2a <- ggplot(est, aes(x = factor(id), y = est, ymin = lb, ymax = ub)) +
   geom_pointrange() +
   geom_hline(yintercept = 0, linetype = "dashed", color = "blue") +
   labs(x = "", y = "VC media consumption estimate", title = "Panel A") +
-  scale_x_discrete(labels = c("Not mar/coh", "Mar/coh", "High school", "Some college", "College degree", 
+  scale_x_discrete(labels = c("Not mar/coh", "Mar/coh", "High school", 
+                              "Some college", "College degree", 
                               "White", "Black", "Hispanic", "Men", "Women")) +
   coord_flip() +
   theme_light() +
   theme(text = element_text(size = 15))
 fig2a
 
-wmdis <- weighted.mean(pew_old$dis, weight = rwt)
+wmdis <- weighted.mean(pew$dis, weight = rwt)
 fig2b <- ggplot(prdis, aes(x = factor(id), y = est, ymin = est - 1.96*se, ymax =  est + 1.96*se)) +
   geom_pointrange() +
   geom_hline(yintercept = wmdis, linetype = "dashed", color = "blue") +
@@ -202,5 +234,20 @@ fig2b
 
 fig2 <- ggarrange(fig2a, fig2b) 
 fig2
-ggsave("fig2.png", plot = fig2)
+ggsave("cmed-fig2.pdf", plot = fig2)
 
+
+
+### Figure for poster
+fig3 <- ggplot(est, aes(x = factor(id), y = est, ymin = lb, ymax = ub)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "blue") +
+  labs(x = "", y = "VC media consumption estimate") +
+  scale_x_discrete(labels = c("Not mar/coh", "Mar/coh", "High school", 
+                              "Some college", "College degree", 
+                              "White", "Black", "Hispanic", "Men", "Women")) +
+  coord_flip() +
+  theme_light() +
+  theme(text = element_text(size = 30))
+fig3
+ggsave("fig3.png", plot = fig3)
